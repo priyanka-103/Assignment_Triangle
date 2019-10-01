e78ec58c3bef25557e7c6a6aba86b77fdadc70d3
priyanka-103 <45679902+priyanka-103@users.noreply.github.com>
2019-10-01 19:05:47 -0400

    Contains complete triangle class

---
+++ TriangleSolver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Assignment2
+{
+    public static class TriangleSolver
+    {
+        public static string Analyze(int side1, int side2, int side3)
+        {
+            string outputString = "";
+
+            if (side1 + side2 <= side3 || side1 + side3 <= side2 || side3 + side2 <= side1)
+            {
+                outputString = "Output:\nDoesn't form a triangle\n";               
+            }
+            
+            else
+            {
+                outputString = "Output:\nForms a triangle";
+
+                if (side1 != side2 && side2 != side3 && side1 != side3)
+                {
+                    outputString = String.Concat(outputString, "\nThis Triangle is Scalene\n\n");
+                }
+                else if (((side1 == side2) && (side1 != side3)) || ((side2 == side3) && (side2 != side1)) || ((side1 == side3) && (side1 != side2)))
+                {
+                    outputString = String.Concat(outputString, "\nThis Triangle is Isosceles\n\n");
+                }
+                else if (side1==side2 && side2==side3)
+                {
+                    outputString=String.Concat(outputString, "\nThis Triangle is Equilateral\n\n");
+                }
+                
+            }
+
+
+            return outputString;
+        }
+
+
+    }
+}
